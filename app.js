e5dbbb410aa0210e352400941cdcf56d33d0a78b
natasyathe <103834940+natasyathe@users.noreply.github.com>
2022-04-17 17:40:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const HOST = '0.0.0.0'
 const app = express()
 app.get('/', (req, res) => {
     res.send('Nama: Natasya The\n')
+app.get('/', (req, res) => {
     res.send('NIM: 52019022\n')
+app.get('/', (req, res) => {
     res.send('Jurusan: Teknik Informatika\n')
 })
 
